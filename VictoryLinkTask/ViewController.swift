51a3228d7e5a4e967a3e2a9363e4a1e39c421dd7
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2019-09-18 10:22:18 -0700

    Initial Commit

---
+++ VictoryLinkTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VictoryLinkTask
+//
+//  Created by Ahmed M. Hassan on 9/18/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
