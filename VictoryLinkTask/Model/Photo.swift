f65495788b071926efc326628250ddd5aec9a5a1
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2019-09-18 14:11:46 -0700

    model added (without serialization)

---
+++ VictoryLinkTask/Model/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  VictoryLinkTask
+//
+//  Created by Ahmed M. Hassan on 9/18/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    
+    var id: String
+    var title: String
+    var photoSizes: [PhotoSize]
+}
---
+++ VictoryLinkTask/Model/PhotoSizes.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSizes.swift
+//  VictoryLinkTask
+//
+//  Created by Ahmed M. Hassan on 9/18/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoSize {
+    
+    var width: Int
+    var height: Int
+    var media: String
+    
+}
